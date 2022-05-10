4e04bbe5f8a2189811f91d34e61a488aa91cea70
TheBrenny <iam@justbrenny.me>
2022-05-11 09:19:10 +0930

    update deps

--- test/runTest.js
+++ test/runTest.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const { runTests } = require('vscode-test');
+const { runTests } = require('@vscode/test-electron');
 
 async function main() {
 	try {
@@ -15,6 +15,7 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+		console.error(err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
